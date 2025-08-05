6628ec429caf34a236f95ddd01814dc970129997
Abdul Hanan <a.hananwork4@gmail.com>
2025-08-05 18:08:08 +0500

    Update llm_instance.py

--- utils/llm_instance.py
+++ utils/llm_instance.py
@@ -1,9 +1,10 @@
 from langchain_google_genai import ChatGoogleGenerativeAI
 import os
 
-os.environ["GOOGLE_API_KEY"] = "AIzaSyBeNzRvS4QfhU1em-EGZFLhy4PpUB4rrGE"
+os.environ["GOOGLE_API_KEY"] = "API_KEY_HERE"
 
 llm = ChatGoogleGenerativeAI(
     model="gemini-2.0-flash",
     temperature=0.4
+
 )
